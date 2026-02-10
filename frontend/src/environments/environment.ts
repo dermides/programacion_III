3d2520b6d6959ec426e27d5fbbf0b9d3eafe466b
Dermides <djbc29@gmail.com>
2026-02-10 10:05:13 -0400

    Agregado ambiente de desarrollo y produccion

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3000',
+  SERVER_URL: 'https://backend-2ydc.onrender.com',
+  CLIENT_URL: 'https://inspiring-blini-8ae4fd.netlify.app/',
 };
