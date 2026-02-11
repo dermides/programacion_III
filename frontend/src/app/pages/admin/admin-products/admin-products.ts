92ea089babe7e808dfc7b25c0364b2a887beebe7
Dermides <djbc29@gmail.com>
2026-02-11 17:50:56 -0400

    Revision Crud

--- frontend/src/app/pages/admin/admin-products/admin-products.ts
+++ frontend/src/app/pages/admin/admin-products/admin-products.ts
@@ -4,6 +4,7 @@ import { Product } from '../../../models';
 import { RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
 import { NotificationService } from '../../../services/notification';
+import { signal } from '@angular/core'; // Importar signal
 
 @Component({
   selector: 'app-admin-products',
@@ -16,7 +17,9 @@ export class AdminProductsComponent implements OnInit {
   private productService = inject(ProductService);
   private notification = inject(NotificationService);
 
-  products: Product[] = [];
+  //products: Product[] = [];
+  products = signal<Product[]>([]);
+  allProducts= signal<Product[]>([]);
 
   ngOnInit() {
     this.loadProducts();
@@ -26,8 +29,38 @@ export class AdminProductsComponent implements OnInit {
     /*this.productService.getAll().subscribe(data => {
       this.products = data;
     });*/
-    this.productService.getAll().subscribe((response: any) => this.products = response.data);
+    //this.productService.getAll().subscribe((response: any) => this.products = response.data);
+    // Cambiamos el tipo a 'any' temporalmente para evitar que TypeScript se queje
+    this.productService.getAll().subscribe({
+    next: (data) => {
+      this.allProducts.set(data);
+      this.products.set(data);
+
+    }
+  });
   }
+
+  filterProducts(event: any) {
+  const query = event.target.value.toLowerCase(); // Lo que escribió el usuario
+
+  if (!query) {
+    // Si borró todo, volvemos a mostrar la lista original completa
+    this.products.set(this.allProducts());
+    return;
+  }
+
+  // Filtramos por ID (o por nombre si quieres también)
+  this.products.set(this.allProducts().filter(product => {
+    // Convertimos el ID a string para poder buscar
+    const idCoincide = product.id.toString().includes(query);
+    const nombreCoincide = product.name.toLowerCase().includes(query);
+
+    // Retorna TRUE si coincide el ID o el Nombre
+    return idCoincide || nombreCoincide;
+  }));
+}
+
+
 
   deleteProduct(id: number) {
     if (confirm('¿Estás seguro de que quieres eliminar este producto?')) {
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  apiUrl: 'https://backend-2ydc.onrender.com',
   SERVER_URL: 'http://localhost:3000',
   CLIENT_URL: 'http://localhost:4200',
 };
